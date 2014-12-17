60718a5ca164589ed5b149f8b9b5730955883c01
Michelle Glauser <michelleglauser@gmail.com>
2014-12-17 12:36:52 -0800

    "Create CSS and HTML files, set index URL and view, add staticfiles to base settings and .gitignore."

---
+++ PM_app/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from <appfoldername> import views
+from django.conf import settings
+
+if settings.DEBUG:
+    urlpatterns = patterns('',
+        url(r'^$', views.index, name='index')
+        url(r'^$', 'PM_app.views.index', name='home'),
+    )
