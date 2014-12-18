718f24e2762aa79fc0fb6d5b29ee2e61e223d764
Michelle Glauser <michelleglauser@gmail.com>
2014-12-17 17:40:27 -0800

    Create edit_task view and link to edit on project page.

--- PM_app/views.py
+++ PM_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 from PM_app.models import Project, Task
 from .forms import ProjectForm, TaskForm
 
@@ -12,7 +12,8 @@ def list_projects(request):
 
 def show_project(request, pk):
 	project = get_object_or_404(Project, pk=pk)
-	return render(request, 'show_project.html', {'project': project})
+	tasks = get_list_or_404(Task)
+	return render(request, 'show_project.html', {'project': project, 'tasks': tasks})
 
 def create_project(request):
 	if request.method == "POST":
@@ -35,3 +36,14 @@ def create_task(request):
 	else:
 		form = TaskForm()
 	return render(request, 'create_task.html', {'form': form})
+
+def edit_task(request):
+	if request.method == "POST":
+		form = TaskForm(request.POST)
+		if form.is_valid():
+			task = form.save(commit=False)
+			task.save()
+			return redirect('PM_app.views.show_project', pk=project.pk)
+	else:
+		form = TaskForm()
+	return render(request, 'edit_task.html', {'form': form})	
